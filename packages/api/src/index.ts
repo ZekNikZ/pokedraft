4538ea5f897c898efeade4ddfc6bbaf2b53d69cf
Matthew McCaskill <mattrmccaskill@gmail.com>
2023-07-04 11:33:20 -0500

    api app setup

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,12 +1,26 @@
+import express from "express";
 import { createDataSource } from "pokedraft-types";
 import { exit } from "process";
 
-const db = createDataSource();
+const db = createDataSource({
+  logging: false,
+});
 try {
   await db.initialize();
-  exit(1);
 } catch (error) {
   console.error("Could not connect to database.");
   console.error(error);
+  exit(1);
 }
 console.log("Connected to database!");
+
+const app = express();
+const port = 4000;
+
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
